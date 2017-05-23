318a6247fbbda925c0269072d0002e8c173fc4f8
Type in your name <Type in your email>
2017-05-23 08:21:35 +0700

    first commit

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message < ApplicationRecord
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+  belongs_to :user
+  belongs_to :chat_room
+
+  validates :body, presence: true, length: {minimum: 2, maximum: 100}
+
+  def timestamp
+    created_at.strftime('%H:%M:%S %d %B %Y')
+  end
+end
